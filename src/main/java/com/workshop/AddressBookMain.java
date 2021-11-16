97a3f157db2d0bdf0a53a4cf46adc71db91a756f
Shivammishra789 <shivammishra7789@gmail.com>
2021-11-16 12:00:07 +0530

    ADDED: creating contact in addressBook

--- src/main/java/com/workshop/AddressBookMain.java
+++ src/main/java/com/workshop/AddressBookMain.java
@@ -5,5 +5,11 @@ public class AddressBookMain {
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to address book program");
+		
+		ContactDetails contactDetails = new ContactDetails("Shivam","Mishra","Pimpri","Pune","Mah","411039","8830466378","Shivam@gmail.com");
+		
+		System.out.println(contactDetails);
+
 	}
+	
 }
---
+++ src/main/java/com/workshop/ContactDetails.java
@@ -0,0 +1,26 @@
+package com.workshop;
+
+public class ContactDetails {
+
+	//variables
+	private String firstName, lastName, address, city, state, zip, phoneNo, emailId;
+
+	public ContactDetails(String firstName, String lastName, String address, String city, String state, String zip,
+			String phoneNo, String emailId) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.address = address;
+		this.city = city;
+		this.state = state;
+		this.zip = zip;
+		this.phoneNo = phoneNo;
+		this.emailId = emailId;
+	}
+
+	@Override
+	public String toString() {
+		return "ContactList [firstName=" + firstName + ", lastName=" + lastName + ", address=" + address + ", city="
+				+ city + ", state=" + state + ", zip=" + zip + ", phoneNo=" + phoneNo + ", emailId=" + emailId + "]";
+	}
+}
